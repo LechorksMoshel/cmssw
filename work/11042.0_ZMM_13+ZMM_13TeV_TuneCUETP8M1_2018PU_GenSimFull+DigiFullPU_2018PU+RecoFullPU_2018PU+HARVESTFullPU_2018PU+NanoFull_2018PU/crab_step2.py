a6513dd75f02d6babbb6f70b6a32d850b2bf7126
Siqi <yuansiqi0114@hotmail.com>
2019-04-02 12:35:15 -0500

    Updated jet performance measurement for ZMM samples

--- work/11042.0_ZMM_13+ZMM_13TeV_TuneCUETP8M1_2018PU_GenSimFull+DigiFullPU_2018PU+RecoFullPU_2018PU+HARVESTFullPU_2018PU+NanoFull_2018PU/crab_step2.py
+++ work/11042.0_ZMM_13+ZMM_13TeV_TuneCUETP8M1_2018PU_GenSimFull+DigiFullPU_2018PU+RecoFullPU_2018PU+HARVESTFullPU_2018PU+NanoFull_2018PU/crab_step2.py
@@ -4,25 +4,29 @@ from WMCore.Configuration import Configuration
 config = Configuration()
 
 config.section_("General")
-config.General.requestName = 'ZMM_GEN'
+config.General.requestName = 'ZMM_DIGI_L1_DIGI2RAW_HLT_PU'
 config.General.workArea = 'Crab3Area'
 
 config.section_("JobType")
-config.JobType.pluginName = 'PrivateMC'
+config.JobType.pluginName = 'Analysis'
 #config.JobType.inputFiles = ['step2_DIGI_L1_DIGI2RAW_HLT_PU.py','step3_RAW2DIGI_L1Reco_RECO_RECOSIM_EI_PAT_VALIDATION_DQM_PU.py']
 #config.JobType.disableAutomaticOutputCollection = True
 #config.JobType.outputFiles = ['step3_inMINIAODSIM.root']
-config.JobType.psetName = 'ZMM_13TeV_TuneCUETP8M1_cfi_GEN_SIM.py'
+config.JobType.psetName = 'step2_DIGI_L1_DIGI2RAW_HLT_PU.py'
+config.JobType.sendPythonFolder = True
+config.JobType.maxMemoryMB = 4500 #step2 turn out to use a lot
+#config.JobType.numCores = 2
 #config.JobType.allowUndistributedCMSSW = True
 
 config.section_("Data")
-config.Data.splitting = 'EventBased'
+config.Data.splitting = 'EventAwareLumiBased'
 config.Data.unitsPerJob = 200 #number of events per jobs
-config.Data.totalUnits = 1000000 #number of event
+config.Data.totalUnits = -1 #number of event
+config.Data.inputDBS = 'phys03'
+config.Data.inputDataset = '/ZMM_PU_HEDepth_GEN/syuan-ZMM_PU_HEDepth_GEN-01b325d492eb745aa9f7609172ddfa01/USER'
 config.Data.outLFNDirBase = '/store/user/syuan/ZMM_UPGRADE'  
 config.Data.publication = True
-config.Data.outputDatasetTag = 'QCD_FlatPT_2018_PU_HEDepth_GEN'
-config.Data.outputPrimaryDataset = 'QCD_FlatPT_2018_PU_HEDepth_GEN'
+config.Data.outputDatasetTag = 'ZMM_PU_HEDepth_step2'
 #config.Data.lumiMask = ''
 
 config.section_("Site")
